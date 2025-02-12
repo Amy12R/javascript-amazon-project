b338a580aaf235f4b0073d6b65782d5491e257b0
Amy12R <rehobothgetachewabegaz18@gmail.com>
2025-02-12 22:26:57 +0530

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,57 @@
+import { formatCurrency } from '../scripts/utils/money.js'; 
+
+console.log('test suite: formatCurrency');
+
+// Give each of these test cases a name.
+console.log('converts cents into dollars'); 
+
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed'); 
+}
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+} // a test case
+
+console.log('rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed'); 
+}
+
+console.log('rounds down to the nearest cent');
+
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('passed');
+} else {
+  console.log('failed'); 
+}
+/* 
+Easiest way to test:
+- Open the website and try out the code
+This is called manual testing. 
+Advantage: 
+- It is really useful for quickly checking to see if everything is working. 
+Disadvantages of Manual Testing:
+1. Hard to test every situation
+2. Hard to re-test the code
+
+Automated testing = using code to test code
+
+2 Types of Test Cases:
+1. Basic test csses = tests if the code is working or not
+2. Edge cases = test with values that are a little bit tricky, in our case, the last two cases are edge cases. 
+
+Automated test make it really easy to retest our code after we make any changes.
+
+When we do testing, we group related tests together. 
+A group of related tests is called a test suite.  
+*/
